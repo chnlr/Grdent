01ffbed17e7e2c6ecc9bc7c5432d03913fca4a57
chnlr <ch@ndler.lol>
2024-09-26 20:04:26 -0500

    remove download

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let previousGradient = '';
+let previousGradient = '';  // Declare once at the top of your script
 
 function updateGradient() {
   const color1 = document.getElementById("color1").value;
@@ -56,42 +56,3 @@ function downloadGradient() {
   link.click();
 }
 
-function downloadVideo() {
-    var video = new Whammy.Video(30);  // 30 FPS for a smooth video
-  
-    const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 400;
-  
-    const color1 = document.getElementById("color1").value;
-    const color2 = document.getElementById("color2").value;
-  
-    // Create 5 seconds of frames at 30 FPS
-    const totalFrames = 30 * 5;  // 30 FPS * 5 seconds = 150 frames
-  
-    for (let i = 0; i < totalFrames; i++) {
-        const angle = (i / totalFrames) * 360;  // Gradually change the angle
-  
-        // Create the gradient for the current frame
-        const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
-        gradient.addColorStop(0, color1);
-        gradient.addColorStop(1, color2);
-  
-        // Fill the canvas with the gradient
-        ctx.fillStyle = gradient;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-  
-        // Add the current canvas frame to the video
-        video.add(ctx.getImageData(0, 0, canvas.width, canvas.height));
-    }
-  
-    // Render and download the video
-    video.compile(false, function(output) {
-        var url = window.URL.createObjectURL(output);
-        var link = document.createElement('a');
-        link.href = url;
-        link.download = 'moving-gradient.webm';  // Downloads as .webm video
-        link.click();
-    });
-}
